fc8676a13953c9190f67807f57e8a93d77877185
AustinP <rockmanvnx6@gmail.com>
2018-02-10 20:50:40 +0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // html tag
+  templateUrl: './app.component.html', // html url
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hello world';
+}
